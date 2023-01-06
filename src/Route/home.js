57194b653ae2bea2b4cb2d9efe950e84ba826660
unknown <hamayoon.47611@iqra.edu.pk>
2023-01-06 18:30:05 +0500

    added cartlist file in components folder

--- src/Route/home.js
+++ src/Route/home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+import Cartitem from '../components/cartitem'
 
 function Home() {
   return (
     <>
     <Navbar/>
+    <Cartitem/>
     </>
   )
 }
---
+++ src/components/cartitem.js
@@ -0,0 +1,23 @@
+import './cartitem.css'
+import React from 'react'
+
+function Cartitem() {
+  return (
+    <>
+    <div className='cartitem' >
+        <div className='right' >
+            <img alt='' src='' />
+        </div>
+        <div className='left' >
+            <div>Fan</div>
+            <div>Price:999</div>
+            <div>qty:12</div>
+
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default Cartitem
