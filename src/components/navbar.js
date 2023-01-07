2fa27ef409453471117b005dddee279c093f5d28
unknown <hamayoon.47611@iqra.edu.pk>
2023-01-07 20:02:15 +0500

    complete the project

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './navbar.css'
-import {FaFacebook} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+
 
 
 
@@ -13,6 +12,7 @@ const Navbar = () => {
         </div>
         <div className='Cart-icon'>
             <img alt='cart' src='https://t3.ftcdn.net/jpg/03/13/36/92/240_F_313369233_w50qr2zvY2wGOsV9suFcoifB1JRfZK5M.jpg' />
+            <span>5</span>
         </div>
     </div>
   )
